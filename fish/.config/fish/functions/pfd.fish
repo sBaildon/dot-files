166926232c9821a6610ab5f291b2df578e06abfd
Sean Baildon <sean@baildon.co>
2016-04-16 17:21:45 +0100

    Check OS for certain fish commands

--- fish/.config/fish/functions/pfd.fish
+++ fish/.config/fish/functions/pfd.fish
@@ -1,8 +1,11 @@
 # Return the path of the frontmost Finder window
 
 function pfd -d "Return the path of the frontmost Finder window"
-  osascript 2>/dev/null -e '
-      tell application "Finder"
-        return POSIX path of (target of window 1 as alias)
-      end tell'
+	if uname | grep Darwin > /dev/null
+		osascript 2>/dev/null -e '
+			tell application "Finder"
+				return POSIX path of (target of window 1 as alias)
+			end tell
+		'
+	end
 end
