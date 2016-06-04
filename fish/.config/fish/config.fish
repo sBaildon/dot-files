a0845778980a663190b5162761f369f4463c68ac
Sean Baildon <sean@baildon.co>
2016-06-04 17:58:51 +0100

    add XDG defaults to fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,12 @@
 set -x XDG_CONFIG_HOME "$HOME/.config/"
+set -x XDG_CACHE_HOME "$HOME/.cache/"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
+set -x GEM_HOME "$XDG_DATA_HOME/gem"
+set -x GNUPGNOME "$XDG_DATA_HOME/gnupg"
 
 set -x projects "$HOME/Projects/"
+set -x work "$projects/snakeway/"
 
 set -U fish_user_paths /opt/local/bin /opt/local/sbin
