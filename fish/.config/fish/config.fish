784039159310fcbcd3fa321b5d802f2b8fc43731
Sean Baildon <sean@baildon.co>
2016-07-09 21:19:52 +0100

    Remove unecessary slashes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,16 +13,16 @@ set -x XDG_DATA_DIRS /usr/local/share/:/usr/share/
 set -x XDG_CONFIG_DIRS /etc/xdg
 
 # Configure programs to use XDG spec
-set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/config
-set -x GEM_HOME "$XDG_DATA_HOME"/gem
-set -x GNUPGNOME "$XDG_DATA_HOME"/gnupg
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"npm/config
+set -x GEM_HOME "$XDG_DATA_HOME"gem
+set -x GNUPGNOME "$XDG_DATA_HOME"gnupg
 set -x INFINIT_DATA_HOME "$XDG_DATA_HOME"infinit/filesystem/
-set -x LESSHISTFILE "$XDG_DATA_HOME"/less/lesshst
+set -x LESSHISTFILE "$XDG_DATA_HOME"less/lesshst
 
 # Specific options for me
 set -x projects "$HOME"/Projects/
 set -x work "$projects"snakeway/
-set -x GOPATH "$projects"golang
+set -x GOPATH "$projects"golang/
 
 # Set path
-set -U fish_user_paths "$GOPATH"/bin /opt/local/bin /opt/local/sbin
+set -U fish_user_paths "$GOPATH"bin /opt/local/bin /opt/local/sbin
