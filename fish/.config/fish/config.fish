6867d15eaa9ce73bf474c48a152fb317a5f572ad
Sean Baildon <sean@baildon.co>
2017-04-22 14:58:24 +0100

    caddy xdg

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,6 +31,7 @@ set -x SCREENRC "$XDG_CONFIG_HOME"screen/screenrc
 set -x PYTHON_EGG_CACHE "$XDG_CACHE_HOME"python-eggs
 set -x WINEPREFIX "$XDG_DATA_HOME"wine
 set -x TERMINFO "$XDG_DATA_HOME"terminfo
+set -x CADDYPATH "$XDG_DATA_HOME"caddy
 
 # Load extra files
 for f in "$XDG_CONFIG_HOME"fish/extras/*.fish
