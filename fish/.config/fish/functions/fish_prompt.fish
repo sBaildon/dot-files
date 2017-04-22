90f32d0567240a804c63532fa1d7ac1177d55680
Sean Baildon <sean@baildon.co>
2017-04-22 12:27:44 +0100

    testing jobs prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,10 +1,17 @@
 function fish_prompt
   if [ $status != 0 ]
-  	set_color $fish_color_error
+    set_color $fish_color_error
   else
-	  set_color $fish_color_cwd
+    set_color $fish_color_cwd
   end
   echo -n (prompt_pwd)
+
+  set -l jobs (jobs | wc -l | xargs)
+  if [ "$jobs" != "0" ]
+    set_color yellow
+    echo -n " [$jobs]"
+  end
+
   set_color normal
   echo -n ' $ '
 end
