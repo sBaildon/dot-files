ec36158bc11a4016631d54d57760d3074947d1d4
Sean Baildon <sean@baildon.co>
2017-05-13 08:55:14 +0100

    fish: xdg alphabetical sort

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,24 +15,24 @@ set -x XDG_DATA_DIRS /usr/local/share/:/usr/share
 set -x XDG_CONFIG_DIRS /etc/xdg
 
 # Configure programs to use XDG spec
-set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/config
+set -x ATOM_HOME "$XDG_DATA_HOME"/atom
+set -x CADDYPATH "$XDG_DATA_HOME"/caddy
+set -x CARGO_HOME "$XDG_DATA_HOME"/cargo
 set -x GEM_HOME "$XDG_DATA_HOME"/gem/
+set -x GIMP2_DIRECTORY "$XDG_CONFIG_HOME"/gimp
 set -x GNUPGHOME "$XDG_DATA_HOME"/gnupg
+set -x GTK2_RC_FILES "$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+set -x GTK_RC_FILES "$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
+set -x ICEAUTHORITY="$XDG_CACHE_HOME"/ICEauthority
 set -x INFINIT_DATA_HOME "$XDG_DATA_HOME"/infinit/filesystem/
 set -x LESSHISTFILE "$XDG_DATA_HOME"/less/history
 set -x LESSKEY "$XDG_CONFIG_HOME"/less/key
-set -x CARGO_HOME "$XDG_DATA_HOME"/cargo
-set -x GIMP2_DIRECTORY "$XDG_CONFIG_HOME"/gimp
-set -x WEECHAT_HOME "$XDG_CONFIG_HOME"/weechat
-set -x ATOM_HOME "$XDG_DATA_HOME"/atom
-set -x GTK2_RC_FILES "$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
-set -x GTK_RC_FILES "$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
-set -x SCREENRC "$XDG_CONFIG_HOME"/screen/screenrc
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/config
 set -x PYTHON_EGG_CACHE "$XDG_CACHE_HOME"/python-eggs
-set -x WINEPREFIX "$XDG_DATA_HOME"/wine
+set -x SCREENRC "$XDG_CONFIG_HOME"/screen/screenrc
 set -x TERMINFO "$XDG_DATA_HOME"/terminfo
-set -x CADDYPATH "$XDG_DATA_HOME"/caddy
-set -x ICEAUTHORITY="$XDG_CACHE_HOME"/ICEauthority
+set -x WEECHAT_HOME "$XDG_CONFIG_HOME"/weechat
+set -x WINEPREFIX "$XDG_DATA_HOME"/wine
 
 # Load extra files
 for f in "$XDG_CONFIG_HOME"/fish/extras/*.fish
