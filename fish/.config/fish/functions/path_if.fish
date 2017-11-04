926f6889ddeae75f72e5fa79eee89f70f1eeee0b
Sean Baildon <sean@baildon.co>
2017-11-04 18:25:53 +0000

    dont add to path if already in path

--- fish/.config/fish/functions/path_if.fish
+++ fish/.config/fish/functions/path_if.fish
@@ -1,6 +1,11 @@
 function path_if
 	if status --is-login
 		if test -d $argv[1]
+			for val in $PATH
+				if test $val = $argv[1]
+					return
+				end
+			end
 			set -x PATH $argv[1] $PATH
 		end
 	end
