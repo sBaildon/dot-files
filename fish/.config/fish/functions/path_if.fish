7e29597d08e32cc1c42142426a8d1b4d04c0d0af
Sean Baildon <sean@baildon.co>
2017-12-20 22:11:54 +0000

    less indentation

--- fish/.config/fish/functions/path_if.fish
+++ fish/.config/fish/functions/path_if.fish
@@ -1,12 +1,10 @@
 function path_if
-	if status --is-login
-		if test -d $argv[1]
-			for val in $PATH
-				if test $val = $argv[1]
-					return
-				end
+	if status --is-login; and test -d $argv[1]
+		for val in $PATH
+			if test $val = $argv[1]
+				return
 			end
-			set -x PATH $argv[1] $PATH
 		end
+		set -x PATH $argv[1] $PATH
 	end
 end
