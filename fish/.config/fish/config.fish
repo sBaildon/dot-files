ae364a293e1d6461f86d74063063035aa093cc96
Sean Baildon <sean@baildon.co>
2018-08-02 13:53:14 +0100

    tomorrow-night -> tomorrow

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 # Set colours
 if test -d $HOME/.config/base16-shell/; and status --is-interactive
-	eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+	eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow.sh
 end
 set fish_color_command purple
 set fish_color_param cyan
