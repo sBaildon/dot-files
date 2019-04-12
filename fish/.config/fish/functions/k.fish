8c49cfd38573ef12bd5bfc48d6b562c03e0a0a1e
Sean Baildon <sean@baildon.co>
2019-04-12 11:07:53 +0100

    awk after

--- fish/.config/fish/functions/k.fish
+++ fish/.config/fish/functions/k.fish
@@ -1,7 +1,7 @@
 function k
 	switch (echo $argv[1])
 	case "describe"
-		kubectl describe pod (kubectl get pods | rg $argv[2] | awk '{print $1}'  | fzf) | rg "Image:"
+		kubectl describe pod (kubectl get pods | rg $argv[2] | fzf | awk '{print $1}') | rg "Image:"
 	end
 end
 
