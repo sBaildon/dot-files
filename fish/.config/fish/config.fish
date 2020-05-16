d759f1d3ce461762135077da333704c0ca837b78
Sean Baildon <sean@baildon.co>
2020-05-16 17:01:18 +0100

    fix fish user paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -62,7 +62,7 @@ end
 set -x FZF_DEFAULT_OPTS "--color=light --multi"
 
 # Path stuff
-set fish_user_paths ~/.local/bin
+set fish_user_paths ~/.local/bin $fish_user_paths
 set fish_user_paths "$GOPATH"/bin $fish_user_paths
 set fish_user_paths "$XDG_DATA_HOME"/npm/bin $fish_user_paths
 set fish_user_paths "$XDG_DATA_HOME"/cargo/bin $fish_user_paths
@@ -70,7 +70,7 @@ set fish_user_paths "$XDG_DATA_HOME"/bin $fish_user_paths
 set fish_user_paths "$PYENV_ROOT"/shims $fish_user_paths
 set fish_user_paths "$RBENV_ROOT"/shims $fish_user_paths
 set fish_user_paths (python3 -c 'import site; print(site.USER_BASE + "/bin")' | tr -d '\n') $fish_user_paths
-set fish_user_paths ~/.luarocks/bin/
+set fish_user_paths ~/.luarocks/bin/ $fish_user_paths
 
 # Load extra files
 for f in "$XDG_CONFIG_HOME"/fish/{extras,os}/*.fish
