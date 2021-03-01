fc253ce2b7a6c122b91340dfeae7191496b5cef1
Sean Baildon <sean@baildon.co>
2021-03-01 09:28:31 +0000

    wont be using pyenv with nix

--- fish/.config/fish/conf.d/path.fish
+++ fish/.config/fish/conf.d/path.fish
@@ -3,7 +3,6 @@ set fish_user_paths "$GOPATH"/bin $fish_user_paths
 set fish_user_paths "$XDG_DATA_HOME"/npm/bin $fish_user_paths
 set fish_user_paths "$XDG_DATA_HOME"/cargo/bin $fish_user_paths
 set fish_user_paths "$XDG_DATA_HOME"/bin $fish_user_paths
-set fish_user_paths "$PYENV_ROOT"/shims $fish_user_paths
 set fish_user_paths "$RBENV_ROOT"/shims $fish_user_paths
 set fish_user_paths (python3 -c 'import site; print(site.USER_BASE + "/bin")' | tr -d '\n') $fish_user_paths
 set fish_user_paths ~/.luarocks/bin/ $fish_user_paths
