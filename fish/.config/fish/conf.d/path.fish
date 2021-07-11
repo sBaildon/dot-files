0e9b5570c8f0c681550ae98f5adf0031e83a881d
Sean Baildon <sean@baildon.co>
2021-07-11 10:20:36 +0100

    fish_add_path

--- fish/.config/fish/conf.d/path.fish
+++ fish/.config/fish/conf.d/path.fish
@@ -1,7 +1,7 @@
-set fish_user_paths ~/.local/bin $fish_user_paths
-set fish_user_paths "$GOPATH"/bin $fish_user_paths
-set fish_user_paths "$XDG_DATA_HOME"/npm/bin $fish_user_paths
-set fish_user_paths "$XDG_DATA_HOME"/cargo/bin $fish_user_paths
-set fish_user_paths "$XDG_DATA_HOME"/bin $fish_user_paths
-set fish_user_paths (python3 -c 'import site; print(site.USER_BASE + "/bin")' | tr -d '\n') $fish_user_paths
-set fish_user_paths ~/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/ $fish_user_paths
+fish_add_path ~/.local/bin
+fish_add_path "$GOPATH"/bin
+fish_add_path "$XDG_DATA_HOME"/npm/bin
+fish_add_path "$XDG_DATA_HOME"/cargo/bin
+fish_add_path "$XDG_DATA_HOME"/bin
+fish_add_path (python3 -c 'import site; print(site.USER_BASE + "/bin")' | tr -d '\n')
+fish_add_path ~/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/
