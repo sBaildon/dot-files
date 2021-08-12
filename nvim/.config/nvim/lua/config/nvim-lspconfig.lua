35a4868bcf7cef528ae097c2612789bbce681b05
Sean Baildon <sean@baildon.co>
2021-08-12 10:57:13 +0100

    lspconfig

--- nvim/.config/nvim/lua/config/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/config/nvim-lspconfig.lua
@@ -1,5 +1,17 @@
-require('lspconfig').gopls.setup({})
+local on_attach = function(client, bufnr)
+  vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "single"})
+  --vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "single"})
+end
+
+require('lspconfig').gopls.setup({
+	on_attach = on_attach
+})
+
+require('lspconfig').yamlls.setup({
+	on_attach = on_attach
+})
 
 require('lspconfig').elixirls.setup({
-	cmd = { vim.loop.os_homedir().."/.nix-profile/bin/elixir-ls" };
+	cmd = { vim.loop.os_homedir().."/.nix-profile/bin/elixir-ls" },
+	on_attach = on_attach
 })
