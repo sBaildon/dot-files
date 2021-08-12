badc09e81afe7818655e06e53fc9d703278cb8a2
Sean Baildon <sean@baildon.co>
2021-08-12 10:57:06 +0100

    autopairs config

--- nvim/.config/nvim/lua/config/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/config/nvim-autopairs.lua
@@ -1,4 +1,6 @@
-require('nvim-autopairs').setup({
+local npairs = require('nvim-autopairs')
+
+npairs.setup({
 	check_ts = true
 })
 
@@ -6,3 +8,11 @@ require("nvim-autopairs.completion.compe").setup({
 	map_cr = true, --  map <CR> on insert mode
 	map_complete = true -- it will auto insert `(` after select function or method item
 })
+
+
+local endwise = require("nvim-autopairs.ts-rule").endwise
+
+npairs.add_rules({
+	endwise(' do$', 'end', 'elixir', 'do_block')
+})
+
