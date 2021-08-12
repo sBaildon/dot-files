15294dc1e8c730d7bbfa04ea82fd33376b1c0cbf
Sean Baildon <sean@baildon.co>
2021-08-12 10:56:51 +0100

    git signs config

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -2,10 +2,10 @@ require('gitsigns').setup({
 	current_line_blame = true,
 	current_line_blame_position = 'right_align',
 	signs = {
-		add = { text = "▌" },
-		change = { text = "▌" },
-		delete = { text = "▌" },
-		topdelete = { text = "▌" },
-		changedelete = { text = "▌" },
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "-" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
 	}
 })
