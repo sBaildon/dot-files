de36ec4e6139c4e8be7353d6802272a923830f84
Sean Baildon <sean@baildon.co>
2021-08-12 10:55:17 +0100

    set theme better

--- fish/.config/fish/conf.d/theme.fish
+++ fish/.config/fish/conf.d/theme.fish
@@ -2,9 +2,11 @@ if test "$TERM" != "xterm-kitty"
 	exit 0
 end
 
-set -l theme (defaults read -g AppleInterfaceStyle 2> /dev/null)
-if test -n "$theme" -a "$theme" = "Dark"
-	kitty @ set-colors --all --configured ~/.config/kitty/base16-harmonic-dark.conf
+set -l interface_style (defaults read -g AppleInterfaceStyle 2> /dev/null)
+if test -n "$interface_style" -a "$interface_style" = "Dark"
+	set theme ~/.config/kitty/base16-darktooth-256.conf
 else
-	kitty @ set-colors --all --configured ~/.config/kitty/base16-harmonic-light.conf
+	set theme ~/.config/kitty/base16-mexico-light-256.conf
 end
+
+kitty @ set-colors --all --configured $theme
