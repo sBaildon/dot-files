d8107007fb18f771a84b95e0629b2fcfc9fcc40c
Sean Baildon <sean@baildon.co>
2021-08-18 13:47:42 +0100

    fish: fzf better colours

--- fish/.config/fish/functions/fzf.fish
+++ fish/.config/fish/functions/fzf.fish
@@ -1,6 +1,6 @@
 function fzf
 	command fzf \
-		--color=fg:-1,hl:3,gutter:0,query:7:regular,fg+:21:regular,bg+:18,hl+:3:reverse,info:8,border:18 \
+		--color=fg:-1,hl:3,gutter:0,query:7:regular,fg+:21:regular,bg+:18,hl+:3:reverse,info:8,border:8 \
 		--multi \
 		--border=sharp \
 		--info=inline \
