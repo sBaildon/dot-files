2418cb161363f057801ebb4d6d71a94fe8f8c457
Sean Baildon <sean@baildon.co>
2021-08-18 09:43:16 +0100

    nvim: indent-blankline ignore filetypes

--- nvim/.config/nvim/lua/config/indent-blankline.lua
+++ nvim/.config/nvim/lua/config/indent-blankline.lua
@@ -2,5 +2,6 @@ require("indent_blankline").setup({
 	show_trailing_blankline_indent = false,
 	show_first_indent_level = true,
 	use_treesitter = true,
+	filetype_exclude = {"help", "terminal", "packer"}
 	-- show_current_context = true -- not working rn. breaks colourscheme entirely
 })
