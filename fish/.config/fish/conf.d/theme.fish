dbf72fcdad41998c43a9b0747b3b068ed7bc7337
Sean Baildon <sean@baildon.co>
2021-08-21 09:28:27 +0100

    fish: macos (lol) theme

--- fish/.config/fish/conf.d/theme.fish
+++ fish/.config/fish/conf.d/theme.fish
@@ -4,12 +4,12 @@ end
 
 set -l interface_style (defaults read -g AppleInterfaceStyle 2> /dev/null)
 if test -n "$interface_style" -a "$interface_style" = "Dark"
-	set theme ~/.config/kitty/base16-darktooth-256.conf
+	set theme ~/.config/kitty/base16-macos-dark-256.conf
 else
-	set theme ~/.config/kitty/base16-mexico-light-256.conf
+	set theme ~/.config/kitty/base16-macos-light-256.conf
 end
 
-set theme ~/.config/kitty/base16-nord-256.conf
+#set theme ~/.config/kitty/base16-macos-light-256.conf
 
 if status is-interactive
 	kitty @ set-colors --all --configured $theme
