f06cfc60498f439f662d69a38733dfc66ccb1f31
Sean Baildon <sean@baildon.co>
2021-08-22 08:13:55 +0100

    nvim: gitsigns config pdate

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -1,6 +1,9 @@
 require('gitsigns').setup({
 	current_line_blame = true,
-	current_line_blame_position = 'eol',
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol"
+	},
 	signs = {
 		add = { text = "▍" },
 		change = { text = "▍" },
