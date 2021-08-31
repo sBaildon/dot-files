c85426da0ea28d5edb695aad545c4b256dcad3b8
Sean Baildon <sean@baildon.co>
2021-08-31 16:03:27 +0100

    nvim: autopairs with cmp

--- nvim/.config/nvim/lua/config/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/config/nvim-autopairs.lua
@@ -4,11 +4,10 @@ npairs.setup({
 	check_ts = true
 })
 
---[[ require("nvim-autopairs.completion.compe").setup({
+require("nvim-autopairs.completion.cmp").setup({
 	map_cr = true, --  map <CR> on insert mode
-	map_complete = true -- it will auto insert `(` after select function or method item
-}) ]]
-
+	map_complete = true, -- it will auto insert `(` after select function or method item
+})
 
 local endwise = require("nvim-autopairs.ts-rule").endwise
 
