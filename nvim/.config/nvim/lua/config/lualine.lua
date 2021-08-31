3975eb11bd0de93b71cd03691d906215de48a75a
Sean Baildon <sean@baildon.co>
2021-08-31 16:06:09 +0100

    nvim: big lualine update

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -1,16 +1,70 @@
+local present, gps = pcall(require, "nvim-gps")
+local _, lsp_status = pcall(require, "lsp-status")
+
+
+lsp_status.config({
+	indicator_errors = '❗',
+	indicator_warnings = '⚠️',
+	indicator_info = '⚡',
+	indicator_hint = '💡',
+	indicator_separator = '',
+	indicator_ok = '',
+	status_symbol = '',
+})
+
+local lualine_c
+if present then
+	lualine_c =  {
+		{ lsp_status.status },
+		"filename",
+		{ gps.get_location, condition = gps.is_available, lower = false },
+
+	}
+else
+	lualine_c =  {
+		{ lsp_status.status },
+		"filename"
+	}
+end
+
+local function indentation()
+	local indent_style = vim.api.nvim_buf_get_option(0, "expandtab") == true and "Spaces" or "Tabs"
+	local indent_size = vim.api.nvim_buf_get_option(0, "tabstop")
+
+	return indent_style .. ": " .. indent_size
+end
+
+local lualine_b = {
+	"branch",
+	{ 'diff', color_added = "#008080", color_modified = "#800080", color_removed = "#800000" }
+}
+
 require('lualine').setup({
 	options = {
 		icons_enabled = false,
 		lower = true,
-		component_separators = {'', ''},
 		section_separators = {'', ''},
-		theme = "base16_256"
+		component_separators = {'', ''},
+		theme = "base16_256",
+		disabled_filetypes = {
+			"outline"
+		}
 	},
 	sections = {
-		lualine_c = {
-			"filename",
-			{ 'diff', color_added = "#008080", color_modified = "#800080", color_removed = "#800000" }
+		lualine_a = lualine_a,
+		lualine_b = lualine_b,
+		lualine_c = lualine_c,
+		lualine_z = {
+			"progress",
+			"location"
+
+		},
+		lualine_x = {
+			indentation,
+			"encoding",
+			"fileformat",
+			"filetype",
+			separator = "│"
 		}
 	}
-
 })
