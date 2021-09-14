7b98f90a3995fa82d5d3e9d8bf0ca3f7b7756309
Sean Baildon <sean@baildon.co>
2021-09-14 09:24:33 +0100

    nvim: lspconfig more keybinds

--- nvim/.config/nvim/lua/config/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/config/nvim-lspconfig.lua
@@ -28,6 +28,12 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev( { popup_opts = { show_header = false, border = "single" } })<CR>', opts)
 	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next( { popup_opts = { show_header = false, border = "single" } })<CR>', opts)
+	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 
 	if client.resolved_capabilities.document_formatting then
 		buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
