01d31424914068ede487b983a307c67a9af7d038
Sean Baildon <sean@baildon.co>
2021-09-14 09:22:53 +0100

    nvim: blankline ignore more

--- nvim/.config/nvim/lua/config/indent-blankline.lua
+++ nvim/.config/nvim/lua/config/indent-blankline.lua
@@ -2,6 +2,6 @@ require("indent_blankline").setup({
 	show_trailing_blankline_indent = true,
 	show_first_indent_level = true,
 	use_treesitter = true,
-	filetype_exclude = {"help", "terminal", "packer"}
+	filetype_exclude = {"help", "terminal", "packer", "NvimTree", "Outline", "lspinfo"}
 	-- show_current_context = true -- not working rn. breaks colourscheme entirely
 })
