8d39864eb7f0d8e14e702081003bfdd1190eaf1b
Sean Baildon <sean@baildon.co>
2021-09-14 09:23:16 +0100

    nvim: treesitter textobjects are cool

--- nvim/.config/nvim/lua/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/config/nvim-treesitter.lua
@@ -2,6 +2,50 @@ require('nvim-treesitter.configs').setup({
 	ensure_installed = "maintained",
 	highlight = { enable = true },
 	indent = { enable = true },
+	textobjects = {
+		select = {
+			enable = true,
+
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			}
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			}
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+
+			}
+		}
+	}
 	--[[ playground = {
 		enable = true,
 
