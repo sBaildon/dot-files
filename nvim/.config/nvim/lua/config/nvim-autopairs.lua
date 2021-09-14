09e960157bc32ba06da024bc1bd40153b5b32f5d
Sean Baildon <sean@baildon.co>
2021-09-14 22:02:30 +0100

    nvim: autopairs remove elixir (default support now)

--- nvim/.config/nvim/lua/config/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/config/nvim-autopairs.lua
@@ -8,10 +8,3 @@ require("nvim-autopairs.completion.cmp").setup({
 	map_cr = true, --  map <CR> on insert mode
 	map_complete = true, -- it will auto insert `(` after select function or method item
 })
-
-local endwise = require("nvim-autopairs.ts-rule").endwise
-
-npairs.add_rules({
-	endwise(' do$', 'end', 'elixir', 'do_block')
-})
-
