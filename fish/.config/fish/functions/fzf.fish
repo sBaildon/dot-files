521354338ddde7815f783c3b53a1957440b58a22
Sean Baildon <sean@baildon.co>
2021-09-29 11:26:09 +0100

    fish: remove fzf file because config is set via env

--- fish/.config/fish/functions/fzf.fish
+++
@@ -1,9 +0,0 @@
-function fzf
-	command fzf \
-		--color=fg:-1,hl:3,gutter:0,query:7:regular,fg+:21:regular,bg+:18,hl+:3:reverse,info:8,border:8 \
-		--multi \
-		--border=sharp \
-		--preview-window=border-sharp \
-		--info=inline \
-		$argv
-end
