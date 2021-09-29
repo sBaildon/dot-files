f07441d279575b24ed5e80eb7c26367df7c6d11c
Sean Baildon <sean@baildon.co>
2021-09-29 11:21:39 +0100

    nvim: cmp borders

--- nvim/.config/nvim/lua/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/config/nvim-cmp.lua
@@ -4,6 +4,10 @@ cmp.setup({
 	mapping = {
 		['<C-Space>'] = cmp.mapping.complete()
 	},
+	documentation = {
+		border = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
+		winhighlight = 'NormalFloat:Pmenu,FloatBorder:Pmenu'
+	},
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "buffer "},
