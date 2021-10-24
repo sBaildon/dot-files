67e6994a13dc78f334c8c9ad021dcf26c9115d78
Sean Baildon <sean@baildon.co>
2021-10-24 12:07:39 +0100

    nvim autopairs check if cmp available

--- nvim/.config/nvim/lua/config/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/config/nvim-autopairs.lua
@@ -4,7 +4,12 @@ npairs.setup({
 	check_ts = true
 })
 
-require("nvim-autopairs.completion.cmp").setup({
-	map_cr = true, --  map <CR> on insert mode
-	map_complete = true, -- it will auto insert `(` after select function or method item
-})
+-- local cmp_available = pcall(require, "cmp")
+local cmp_available = false
+
+if cmp_available then
+	require("nvim-autopairs.completion.cmp").setup({
+		map_cr = true, --  map <CR> on insert mode
+		map_complete = true, -- it will auto insert `(` after select function or method item
+	})
+end
