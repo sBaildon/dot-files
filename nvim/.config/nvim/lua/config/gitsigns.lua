da3454f5b579ab929a22c27d27aa7c053ebe42b1
Sean Baildon <sean@baildon.co>
2021-12-16 11:06:09 +0000

    nvim: gitsigns check for presence

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -1,15 +1,19 @@
-require('gitsigns').setup({
-	current_line_blame = true,
-	current_line_blame_opts = {
-		virt_text = true,
-		virt_text_pos = "eol"
-	},
-	signs = {
-		add = { text = "▍" },
-		change = { text = "▍" },
-		delete = { text = "▍" },
-		topdelete = { text = "‾" },
-		changedelete = { text = "▍" },
-	},
+local present, gitsigns = pcall(require, "gitsigns")
 
-})
+if present then
+	gitsigns.setup({
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol"
+		},
+		signs = {
+			add = { text = "▍" },
+			change = { text = "▍" },
+			delete = { text = "▍" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "▍" },
+		},
+
+	})
+end
