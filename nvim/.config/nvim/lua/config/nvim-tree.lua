f7646ca4dd171c70de0c087799c5c5b916955b27
Sean Baildon <sean@baildon.co>
2021-12-24 00:19:38 +0000

    nvim: load nvim-tree if plugin installed

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -1,13 +1,16 @@
-local nvim_tree = require("nvim-tree")
+local present, nvim_tree = pcall(require, "nvim-tree")
 
-nvim_tree.setup({
-	view = {
-		auto_resize = false
-	},
-	filters = {
-		dotfiles = true
-	},
-	git = {
-		ignore = true
-	}
-})
+if present then
+	nvim_tree.setup({
+		view = {
+			auto_resize = false
+		},
+		filters = {
+			dotfiles = true
+		},
+		git = {
+			ignore = true
+		}
+	})
+end
+
