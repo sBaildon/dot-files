f679c71926d32dcb1d389882e257d53406cb705b
Sean Baildon <sean@baildon.co>
2022-01-16 10:48:29 +0000

    gpg: dont set auth sock

--- fish/.config/fish/conf.d/gpg.fish
+++ fish/.config/fish/conf.d/gpg.fish
@@ -1,7 +1,9 @@
-if type -q gpg
-	if not test -d $GNUPGHOME
-		mkdir -p -m 700 $GNUPGHOME
-	end
-
-	set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+if ! type --query gpg
+	exit 0
 end
+
+if not test -d $GNUPGHOME
+	mkdir -p -m 700 $GNUPGHOME
+end
+
+# set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
