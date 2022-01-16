9ed866e6b7d8175e98131e89b7b76312ceca3993
Sean Baildon <sean@baildon.co>
2022-01-16 10:48:22 +0000

    fish: xdg_state_home

--- fish/.config/fish/conf.d/000-env.fish
+++ fish/.config/fish/conf.d/000-env.fish
@@ -4,6 +4,7 @@ set -x EDITOR (type --path vi)
 set -x XDG_CONFIG_HOME "$HOME"/.config
 set -x XDG_CACHE_HOME "$HOME"/.cache
 set -x XDG_DATA_HOME "$HOME"/.local/share
+set -x XDG_STATE_HOME "$HOME"/.local/state
 set -x XDG_DATA_DIRS /usr/local/share/:/usr/share
 set -x XDG_CONFIG_DIRS /etc/xdg
 
